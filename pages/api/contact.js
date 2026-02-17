c2bcf057cb530b37cbd43487bdbf326d74d40c61
Yancy-cmc <Yancy@choosemycoverage.com>
2026-02-17 14:22:34 -0700

    Update contact.js

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
 
   try {
     await resend.emails.send({
-      from: "Choose My Coverage <support@choosemycoverage.com>",
+      from: "Choose My Coverage <onboarding@resend.dev>",
       to: "yancy@choosemycoverage.com",
       subject: `New Contact Form Submission - ${topic}`,
       html: `
