2b6cddd69068d571cfcc21918e27e14b8355ee97
Yancy-cmc <Yancy@choosemycoverage.com>
2026-02-20 18:35:28 -0700

    Update apply.js

--- api/apply.js
+++ api/apply.js
@@ -13,6 +13,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
