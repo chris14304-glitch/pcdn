c8227754fa5dd52a4e7200bdc30fc63d96040e88
Yancy-cmc <Yancy@choosemycoverage.com>
2026-02-21 02:45:18 -0700

    Update verify-vin.js

--- api/verify-vin.js
+++ api/verify-vin.js
@@ -1,4 +1,6 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
   const vin = (req.query.vin || '').trim().toUpperCase();
 
   if (!vin || vin.length !== 17) {
