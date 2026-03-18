78ac9e1d71bcfe65dac23b71db9f23ea4ac9a512
Yancy-cmc <Yancy@choosemycoverage.com>
2026-03-18 10:15:11 -0600

    Update zip.js

--- api/zip.js
+++ api/zip.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   // Always set CORS headers first
-  res.setHeader("Access-Control-Allow-Origin", "https://www.choosemycoverage.com");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
