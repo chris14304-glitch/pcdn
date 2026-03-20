ad7495f74b4fd0b357ce781d280c5f4b7e52b549
Yancy-cmc <Yancy@choosemycoverage.com>
2026-03-20 10:08:11 -0600

    Update contact.js

--- api/contact.js
+++ api/contact.js
@@ -43,7 +43,7 @@ export default async function handler(req, res) {
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   } else {
-    res.setHeader("Access-Control-Allow-Origin", "https://www.choosemycoverage.com");
+    res.setHeader("Access-Control-Allow-Origin", "*");
   }
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
