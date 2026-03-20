b58138e4d3a3fc6851db2bfce8e9ab61d76f93c4
Yancy-cmc <Yancy@choosemycoverage.com>
2026-03-20 12:19:18 -0600

    Update claim.js

--- api/claim.js
+++ api/claim.js
@@ -59,7 +59,7 @@ function validateUploadedFile(file) {
 // --- Handler ---
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "https://www.choosemycoverage.com");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
